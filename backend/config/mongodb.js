8c0bcda2458e1d785656210cad93fd480ed1f1f1
Insiyazaidi <167618418+Insiyazaidi@users.noreply.github.com>
2025-11-24 00:15:24 +0530

    Delete backend directory

--- backend/config/mongodb.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const connectdb = async()=>{
- await mongoose.connect(process.env.MONGO_URL)
- console.log("db connected")
-}
-export default connectdb
--- backend/routes/doctorroute.js
+++
@@ -1,6 +0,0 @@
-import express from "express"
-import { doctorlist } from "../controllers/doctorcontroller.js"
-const doctorrouter = express.Router()
-doctorrouter.get("/list" , doctorlist)
-
-export default doctorrouter
